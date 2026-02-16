1c7773604b4401931564a8d0d0b6d79c588b25f8
Ties7 <97968332+Ties7@users.noreply.github.com>
2026-02-16 09:44:12 +0100

    feat: stad in api call #2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
   const apiKey = import.meta.env.PUBLIC_WEATHER_API_KEY;
   
+  let city = '';
   let weatherData = null;
   
-  async function testAPI() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
+  async function searchWeather() {
+    // Check of er iets is ingevuld
+    if (city === '') {
+      alert('Typ eerst een stad!');
+      return;
+    }
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
     const response = await fetch(url);
     const data = await response.json();
@@ -16,8 +23,14 @@
 
 <h1>Weather App</h1>
 
-<button on:click={testAPI}>
-  Test API
+<input 
+  type="text" 
+  bind:value={city}
+  placeholder="Type een stad..."
+/>
+
+<button on:click={searchWeather}>
+  Zoek Weer
 </button>
 
 {#if weatherData}
